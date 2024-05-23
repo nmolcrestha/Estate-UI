a3b59741e4d4fc6f0f07bed987ced822bc850c66
anmolcnc <anmol@curvesncolors.ch>
2024-05-23 20:58:12 +0545

    search bar component responsive design

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
+const types = ["buy", "rent"];
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onCanPlay={() => switchType({ type })}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          min={0}
+          max={10000000}
+          name="minPrice"
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          min={0}
+          max={10000000}
+          name="maxPrice"
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="/search.png" alt="search" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
