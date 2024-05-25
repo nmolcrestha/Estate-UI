033c70e92d2a1ba8c6003e802446294fb54d7bd2
anmolcnc <anmol@curvesncolors.ch>
2024-05-25 13:21:40 +0545

    Added singlePage route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from "./routes/homePage/homePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/list", element: <ListPage /> },
+      { path: "/:id", element: <SinglePage/> },
     ],
   },
 ]);
